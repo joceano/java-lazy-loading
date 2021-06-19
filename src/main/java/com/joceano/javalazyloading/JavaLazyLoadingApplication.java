8c20221331395131b83fbf55cc519c333ae27449
joceano <alves.joceano@gmail.com>
2021-06-18 21:35:25 -0300

    Initial commit

---
+++ src/main/java/com/joceano/javalazyloading/JavaLazyLoadingApplication.java
@@ -0,0 +1,13 @@
+package com.joceano.javalazyloading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLazyLoadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLazyLoadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joceano/javalazyloading/JavaLazyLoadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joceano.javalazyloading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLazyLoadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
